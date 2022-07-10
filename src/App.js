4a3dc28144b0c82854b69f38de666d9ada353794
Praveen Kumar A <pk0128331@gmail.com>
2022-07-10 07:09:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import ContactCard from './components/ContactCard'
+
+const App = () => {
+    const[contactsArray, setContactsArray] = useState([]);
+    let contacts;
+    const contactsGetter = async() => {
+        let response = await fetch('https://randomuser.me/api/?results=5');
+        response = await response.json();
+        contacts = response.results;
+        console.log(contacts);
+        setContactsArray(contacts);
+    }
+
+    useEffect(() => {contactsGetter();}, []);
+
+    return(
+        <div className='main-container'>
+            <h1>Contact List</h1>
+            <div className='card-container'>
+                {
+                    contactsArray.map((contact, index) => <ContactCard key={index} contact={contact}/>)
+                }
+            </div>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './ContactCard.css';
+
+const ContactCard = ({contact}) => {
+    const [displayStatus, setDisplayStatus] = useState(false);
+
+    const clickHandler = () => {
+        console.log('clicked')
+        setDisplayStatus(!displayStatus);
+    }
+
+    const getExtraInfos = () => {
+        return(
+            <>
+                <p>Age : {contact.dob.age}</p>
+                <p>Gender : {contact.gender}</p>
+                <p>Contat No : {contact.cell}, {contact.phone}</p>
+                <p>Country : {contact.location.country}</p>
+            </>
+            
+        )
+    }
+    return(
+        <div className='card'>
+            <div className='image'><img src={contact.picture.large}></img></div>
+            <div className='details'>
+                <p>Name : {contact.name.first+" "+contact.name.last}</p>
+                <p>Email Id: {contact.email}</p>
+                <button onClick={clickHandler} className='button green'>Click for more details</button>
+                {
+                   (displayStatus) === true ? getExtraInfos() : null
+                }
+                
+            </div>
+        </div>
+    )
+}
+
+export default ContactCard;
